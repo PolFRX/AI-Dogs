7a7d5d2142e0eea673a849792aea875b1702169b
Pol frx <pol.2frx@hotmail.fr>
2019-05-23 01:04:33 +0200

    change img as data

--- process_functions.py
+++ process_functions.py
@@ -1,3 +1,15 @@
 import tensorflow as tf
 import pandas as pd
 import numpy as np
+
+def load_preprocess_image(path):
+    """ Load an image and then call process_image to return a dtype """
+    img = tf.read_file(path)
+    return preprocess_image(img)
+
+def preprocess_image(raw_img):
+    """ Decode an image, resize it and then put each value between 0 and 1 """
+    img = tf.image.decode_image(raw_img)
+    img = tf.image.resize_images(img, [192, 192])
+    img = img/255.0
+    return img
