6f5c68f76309a158ce3bf725513eaa1777447d49
Pol frx <pol.2frx@hotmail.fr>
2019-05-30 01:01:07 +0200

    Test model training on 6 breeds

--- files_functions.py
+++ files_functions.py
@@ -41,16 +41,17 @@ def get_files(categories):
     image_path = image_path[permutation]
 
     # Separate training and validation data with 80% of training data and 20% of validation.
-    length_training_data = math.floor(len(labels) * 0.8)
+    length_training_data = math.floor(len(labels) * 0.95)
     labels_training = labels[:length_training_data]
     labels_validation = labels[length_training_data - len(labels):]
     image_path_training = image_path[:length_training_data]
     image_path_validation = image_path[length_training_data - len(labels):]
 
-    number_elements_training = math.floor(len(labels) * 0.9)
+    number_elements_training = math.floor(len(labels) * 0.95)
     number_elements_validation = len(labels) - number_elements_training
 
-    return labels_training, image_path_training, labels_validation, image_path_validation, number_elements_training, number_elements_validation
+    return labels_training, image_path_training, labels_validation, \
+        image_path_validation, number_elements_training, number_elements_validation
 
 
 def get_categories():
@@ -111,7 +112,8 @@ def get_predict_image_path(image_name):
 
 
 def rename_directories_standford():
-    """ Rename all the directories of the standford's dataset to take off ugly parts. """
+    """ Rename all the directories of the standford's dataset
+    to take off ugly parts and thus make better categories for the model. """
 
     path = os.getcwd() + "\\dataset"
     dataset_dirs = os.listdir(path)
@@ -122,3 +124,47 @@ def rename_directories_standford():
         except IndexError:
             continue
     print("End")
+
+
+def get_files_for_some_categories(categories):
+    """ Get files only for 6 categories (golden retriever, english setter,
+    german shepard, collie, pug) to test if the model works with a few breeds """
+    # Take the path to directory "dataset"
+    path = os.getcwd()
+    path = path + "\\dataset"
+    dataset_dirs = os.listdir(path)
+    # Declare the two list which will contain labels and path of the images
+    labels = list()
+    image_path = list()
+
+    # Browse the "dataset" directory to get all paths for images and labels which are the directories names
+    for dirs in dataset_dirs:
+        if(dirs == "pug" or dirs == "English_setter" or dirs == "German_sheperd"
+                or dirs == "golden_retriever" or dirs == "collie" or dirs == "boxer"):
+            dir_path = path + "\\" + dirs
+            for path2, dir, files in os.walk(dir_path):
+                for files_name in files:
+                    labels.append(categories.index(dirs))
+                    image_path.append(dir_path + "\\" + files_name)
+
+    labels = np.array(labels)
+    image_path = np.array(image_path)
+
+    # Shuffle the two arrays with the same permutation
+    permutation = np.random.permutation(len(labels))
+    labels = labels[permutation]
+    image_path = image_path[permutation]
+
+    # Separate training and validation data with 80% of training data and 20% of validation.
+    length_training_data = math.floor(len(labels) * 0.95)
+    labels_training = labels[:length_training_data]
+    labels_validation = labels[length_training_data - len(labels):]
+    image_path_training = image_path[:length_training_data]
+    image_path_validation = image_path[length_training_data - len(labels):]
+
+    number_elements_training = math.floor(len(labels) * 0.95)
+    number_elements_validation = len(labels) - number_elements_training
+
+    return labels_training, image_path_training, labels_validation,\
+        image_path_validation, number_elements_training, number_elements_validation
+
--- training.py
+++ training.py
@@ -1,16 +1,27 @@
 import tensorflow as tf
-import display, process_functions as pf, files_functions as ff
+import display
+import process_functions as pf
+import files_functions as ff
 import tensorflow.keras.models as km
 import tensorflow.keras.layers as layers
-
-tf.enable_eager_execution()
-CATEGORIES = ff.get_categories()
-SIZE = 300
-LEARNING_RATE = 1e-6
-BATCH_SIZE = 38
+import tensorflow.keras.callbacks as call
 
 
-def train(learning_rate, gray_scale=False, epochs=5, batch_size=32, steps_per_epoch=100, model_name="dogs"):
+tf.enable_eager_execution()  # to avoid using Session
+CATEGORIES = ff.get_categories()  # we take all the categories at first
+SIZE = 200  # size of images: SIZE * SIZE
+LEARNING_RATE = 5e-8  # the model's learning rate
+BATCH_SIZE = 1  # the batch size of data to train the model, 36 is good
+MODEL_NAME = "dogs_6"  # the name that will be taken for the model's save
+GRAY_SCALE = False  # to know if we use grayscale images  to feed the model
+DOGS_6 = True  # to know if we take only 6 breeds of dogs to test the model or all breeds
+STEPS_PER_EPOCH = 129  # steps per epoch, 100 is good for large dataset, else use less
+EPOCHS = 6  # number of epochs to train the model
+
+tensorboard = call.TensorBoard(log_dir='logs/{}'.format('dogs6_2'))
+
+
+def train(learning_rate, gray_scale=False, epochs=5, batch_size=32, steps_per_epoch=100, model_name="dogs", dogs_6=False):
     """ Train the model with data contained in the dataset directory.
 
     Args:
@@ -20,36 +31,37 @@ def train(learning_rate, gray_scale=False, epochs=5, batch_size=32, steps_per_ep
         batch_size: the batch size for data to train the model, 20 by default
         steps_per_epoch: number of steps per epoch, 100 by default
         model_name: the name used to save the model, dogs by default
+        dogs_6: boolean to know if we need to use only 6 breeds to test the model
     Return:
         a trained model
     """
 
-    label_training_ds, image_training_ds, label_validation_ds, image_validation_ds\
-        = load_data(CATEGORIES, gray_scale)
+    label_training_ds, image_training_ds, label_validation_ds, image_validation_ds \
+        = load_data(CATEGORIES, gray_scale, dogs_6=dogs_6)
 
     model = km.Sequential()
-    model.add(layers.Conv2D(256, (8, 8), input_shape=(SIZE, SIZE, 3), activation=tf.nn.relu))
-    model.add(layers.MaxPooling2D(pool_size=(2, 2)))
+    model.add(layers.Conv2D(64, (4, 4), input_shape=(SIZE, SIZE, 3)))
+    model.add(layers.MaxPooling2D(pool_size=(2, 2), data_format='channels_last'))
+    model.add(layers.Activation("relu"))
 
-    model.add(layers.Conv2D(92, (8, 8), activation=tf.nn.relu))
-    model.add(layers.MaxPooling2D(pool_size=(2, 2)))
+    model.add(layers.Conv2D(128, (4, 4)))
+    model.add(layers.MaxPooling2D(pool_size=(2, 2), data_format='channels_last'))
+    model.add(layers.Activation("relu"))
 
-    model.add(layers.Conv2D(92, (8, 8), activation=tf.nn.relu))
-    model.add(layers.MaxPooling2D(pool_size=(2, 2)))
-
-    model.add(layers.Conv2D(64, (8, 8), activation=tf.nn.relu))
-    model.add(layers.MaxPooling2D(pool_size=(2, 2)))
-    model.add(layers.Dropout(0.2))
-
-    model.add(layers.Conv2D(64, (8, 8), activation=tf.nn.relu))
-    model.add(layers.MaxPooling2D(pool_size=(2, 2)))
+    model.add(layers.Conv2D(256, (4, 4)))
+    model.add(layers.MaxPooling2D(pool_size=(2, 2), data_format='channels_last'))
+    model.add(layers.Activation("relu"))
     model.add(layers.Dropout(0.2))
 
     model.add(layers.Flatten())
     model.add(layers.Dense(320, activation=tf.nn.relu))
 
     model.add(layers.Flatten())
-    model.add(layers.Dense(240, activation=tf.nn.relu))
+    model.add(layers.Dense(5000, activation=tf.nn.relu))
+
+    model.add(layers.Dense(2500, activation=tf.nn.relu))
+
+    model.add(layers.Dense(1000, activation=tf.nn.relu))
     model.add(layers.Dropout(0.3))
 
     model.add(layers.Flatten())
@@ -64,8 +76,15 @@ def train(learning_rate, gray_scale=False, epochs=5, batch_size=32, steps_per_ep
     validation_ds = tf.data.Dataset.zip((image_validation_ds, label_validation_ds))
     # model.fit(training_ds, batch_size=batch_size, epochs=epochs, steps_per_epoch=steps_per_epoch,
     #          validation_data=validation_ds, validation_steps=100)
-    model.fit(training_ds, batch_size=batch_size, epochs=epochs, steps_per_epoch=steps_per_epoch)
-    validation_loss, validation_accuracy = model.evaluate(validation_ds, steps=300)
+
+    model.fit(training_ds, batch_size=batch_size, epochs=epochs,
+              steps_per_epoch=steps_per_epoch, callbacks=[tensorboard])
+
+    if dogs_6:
+        validation_loss, validation_accuracy = model.evaluate(validation_ds, steps=41)
+    else:
+        validation_loss, validation_accuracy = model.evaluate(validation_ds, steps=300)
+
     print("Validation_loss: {}, Validation_accuracy: {}".format(validation_loss, validation_accuracy))
 
     ff.save_model(model, model_name)
@@ -73,19 +92,24 @@ def train(learning_rate, gray_scale=False, epochs=5, batch_size=32, steps_per_ep
     return model
 
 
-def load_data(categories, gray_scale):
+def load_data(categories, gray_scale, dogs_6):
     """ Load all datas from the dataset
 
         Args:
             categories: all the dogs breeds
-            gray_scale: to know if we need to take grayscale images for inputs
+            gray_scale: boolean to know if we need to take grayscale images for inputs
+            dogs_6: boolean to know if we need to take only 6 breeds of dogs to test the model
         Return:
             label_ds: tf.data.Dataset containing each label for image_ds (already shuffled)
             image_ds: tf.data.Dataset containing each image in the same order than labels (already shuffled)
     """
 
-    labels_training, images_path_training, labels_validation, images_path_validation, \
-        number_elements_training, number_elements_validation = ff.get_files(categories)
+    if dogs_6:
+        labels_training, images_path_training, labels_validation, images_path_validation, \
+            number_elements_training, number_elements_validation = ff.get_files_for_some_categories(categories)
+    else:
+        labels_training, images_path_training, labels_validation, images_path_validation, \
+            number_elements_training, number_elements_validation = ff.get_files(categories)
 
     # Essai d'appliquer une fonction à tout un numpy array
     # try:
@@ -150,7 +174,7 @@ def predict_image_with_model(image_name, model_name, is_display=True):
 
     prediction = model.predict([image])
     prediction = tf.argmax(prediction)
-    prediction = CATEGORIES.index(prediction)
+    prediction = CATEGORIES(prediction)
     print(prediction)
 
     if is_display:
@@ -158,4 +182,7 @@ def predict_image_with_model(image_name, model_name, is_display=True):
 
 
 # labels_tr, image_tr, labels_val, image_val = load_data(CATEGORIES)
-train(LEARNING_RATE, gray_scale=False, batch_size=BATCH_SIZE)
+# train(LEARNING_RATE, steps_per_epoch=STEPS_PER_EPOCH, gray_scale=GRAY_SCALE, batch_size=BATCH_SIZE,
+#      model_name=MODEL_NAME, dogs_6=DOGS_6, epochs=EPOCHS)
+
+predict_image_with_model('blabla.jpg', 'dogs_6')
